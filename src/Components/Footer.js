145a59ef3bc978f0695033426898e363c69ff685
Kat Leight <leightkt@gmail.com>
2021-03-24 12:29:16 -0600

    add footer information and styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,19 @@
 import './Footer.css'
+import Github from '../Assets/github.png'
+import Blockimage from '../Assets/block.jpeg'
 
 function Footer () {
 
     return(
         <footer>
+            <a href="https://github.com/leightkt"><img src={Github} alt="github logo" className="github-logo" /></a>
             <p>About the Dev</p>
+            <div className="about-dev">
+                <p>Kat Leight</p>
+                <p>Full Stack Software Engineer and Full Stack Climber in Denver, CO</p>
+                <p>Explorer of mountains and new technology</p>
+            </div>
+            <img src={Blockimage} alt="an orange block" className="block-image" />
         </footer>
     )
 }
