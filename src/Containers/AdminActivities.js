d8995c7f167da6ed5a53e1a0840b90f390571d53
Kat Leight <leightkt@gmail.com>
2021-03-26 11:06:15 -0600

    add concensus and verify actions for admin page

--- src/Containers/AdminActivities.js
+++ src/Containers/AdminActivities.js
@@ -2,22 +2,40 @@ import './AdminActivities.css'
 import AddANode from '../Components/AddANode'
 import { useState, useEffect } from 'react'
 
+const backendNodeURL = 'http://localhost:3001'
 
 function AdminActivities () {
     const [ nodes, setNodes ] = useState([])
+    const [ currentNode, setcurrentNode ] = useState(backendNodeURL)
+    const [ message, setMessage ] = useState("")
 
     useEffect(() => {
-        // run fetch here
+        fetch(`${backendNodeURL}/nodes`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+                "Authorization": `${localStorage.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(result => {
+                if(result.length > 0) {
+                    setNodes(result)
+                }
+            })
     }, [])
 
     const handleClick = (event) => {
         event.stopPropagation()
+        setMessage("")
+        const URL = event.target.parentNode.getAttribute("id")
         switch(event.target.innerText) {
             case "VERIFY CHAIN":
-                verifyChain()
+                verifyChain(URL)
                 break
             case "RUN CONCENSUS":
-                runConensus()
+                runConcensus(URL)
                 break
             default:
                 return null
@@ -25,23 +43,57 @@ function AdminActivities () {
 
     }
 
-    const verifyChain = () => {
-
+    const verifyChain = (URL) => {
+        fetch(`${URL}/validateChain`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+                "Authorization": `${localStorage.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(result => {
+                setMessage(result.message)
+            })
     }
 
-    const runConensus = () => {
-
+    const runConcensus = (URL) => {
+        fetch(`${URL}/concensus`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+                "Authorization": `${localStorage.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(result => {
+                setMessage(result.message)
+            })
     }
 
     const displayNodes = () => {
-
+        const allNodes = [ currentNode, ...nodes ]
+        return allNodes.map(node => {
+            return ( 
+                <div key={ node } id={ node }>
+                    <p>Node: { node }</p>
+                    { node === allNodes[0] 
+                        ? <p>Current Node</p>
+                        : null 
+                    }
+                    <button className="admin-button">VERIFY CHAIN</button>
+                    <button className="admin-button">RUN CONCENSUS</button>
+                </div> 
+            )
+        })
     }
 
     return (
-        <section className="admin-section" onClick={ handleClick }>
-            <div className="nodes">{nodes ? displayNodes : null }</div>
-            <button className="admin-button">VERIFY CHAIN</button>
-            <button className="admin-button">RUN CONCENSUS</button>
+        <section className="admin-section">
+            { message ? <p className="errors admin-message">{ message }</p> : null }
+            <div className="nodes" onClick={ handleClick } >{ currentNode ? displayNodes() : null }</div>
             <AddANode />
         </section>
     )
